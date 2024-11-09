8e71a69735892fa8c80f29a0d16bcda8376ba9dc
Gerico Mangali <gmangali@seekasia.com>
2024-11-09 19:19:46 +0800

    hotfix auth

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
-// firebase.js
-const admin = require('firebase-admin');
-const serviceAccount = require('/api/config/serviceAccountKey.json');  // Replace with actual path
+// // firebase.js
+// const admin = require('firebase-admin');
+// const serviceAccount = require('/api/config/serviceAccountKey.json');  // Replace with actual path
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://intellipark-db283.firebaseio.com'  // Replace with your actual Firebase Database URL
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: 'https://intellipark-db283.firebaseio.com'  // Replace with your actual Firebase Database URL
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-module.exports = { db };
+// module.exports = { db };
