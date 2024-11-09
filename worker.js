b4bb29d724539c859a142e003403a17b4688b4ce
Gerico Mangali <gmangali@seekasia.com>
2024-11-09 19:37:10 +0800

    hotfix for auth 2

--- worker.js
+++ worker.js
@@ -1,33 +1,33 @@
 // Load environment variables from .env file
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const { workerData, parentPort } = require('worker_threads');
 const admin = require('firebase-admin');
 
-// dotenv.config();
-// // Ensure Firebase Admin SDK is initialized (this block ensures it initializes only once)
-// if (!admin.apps.length) {
-// // Firebase setup using environment variables
-// const serviceAccount = {
-//     type: "service_account",
-//     project_id: process.env.FIREBASE_PROJECT_ID,
-//     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-//     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Correct any newlines in the private key
-//     client_email: process.env.FIREBASE_CLIENT_EMAIL,
-//     client_id: process.env.FIREBASE_CLIENT_ID,
-//     auth_uri: "https://accounts.google.com/o/oauth2/auth",
-//     token_uri: "https://oauth2.googleapis.com/token",
-//     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-//     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-// };
+dotenv.config();
+// Ensure Firebase Admin SDK is initialized (this block ensures it initializes only once)
+if (!admin.apps.length) {
+// Firebase setup using environment variables
+const serviceAccount = {
+    type: "service_account",
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Correct any newlines in the private key
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: process.env.FIREBASE_DATABASE_URL
-// });
-// }
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+}
 
-// const db = admin.firestore();
-// module.exports = { db };
+const db = admin.firestore();
+module.exports = { db };
 
 const MAX_SLOTS = 50;
 
